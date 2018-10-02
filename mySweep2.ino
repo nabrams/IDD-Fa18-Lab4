879469c9bce1c3fcba799a8d3ae33a8786daf42d
nabrams <nabrams94@gmail.com>
2018-10-02 14:35:04 -0400

    Add files via upload

--- mySweep2.ino
+++ mySweep2.ino
@@ -21,12 +21,12 @@ int dark;
 
 // notes in the melody:
 int melody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+  NOTE_A3, NOTE_G3, NOTE_A3, NOTE_G3, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4
 };
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {
-  4, 8, 8, 4, 4, 4, 4, 4
+  4, 8, 8, 4, 4, 4, 4, 4, 8 , 8, 2 ,4 ,4, 8, 4, 8, 4, 2
 }; 
 
 void setup() {
@@ -38,9 +38,9 @@ void setup() {
 void loop() {
   fsrReading = analogRead(fsrAnalogPin);
   Serial.println(analogRead(A0));
-  if (fsrReading > 1020){
+  if (fsrReading > 800){
     if (dark == 1){
-     for (int thisNote = 0; thisNote < 8; thisNote++) {
+     for (int thisNote = 0; thisNote < 18; thisNote++) {
 
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
